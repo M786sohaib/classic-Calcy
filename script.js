ea09949534c2648dde30deea84f127d892c264c8
Sohaib <m786sohaib@gmail.com>
2025-03-15 21:00:57 -0400

    ADD: functions to display current equation on screen. WIP: function to calculate the value input from screen.

--- script.js
+++ script.js
@@ -6,18 +6,22 @@
 // }
 
 function add(...arr) {
-  return arr.reduce((sum, num) => sum + num, 0);
+  return arr.flat().reduce((sum, num) => sum + num, 0);
 }
 
 function subtract(...arr) {
-  return arr.reduce((sum, num) => sum - num);
+  return arr.flat().reduce((sum, num) => sum - num);
 }
 function multiply(...arr) {
-  return arr.reduce((sum, num) => sum * num, 1);
+  return arr.flat().reduce((sum, num) => sum * num, 1);
 }
 
 function divide(...arr) {
-  return arr.reduce((sum, num) => sum / num);
+  return arr.flat().reduce((sum, num) => sum / num);
+}
+
+function percentage(num) {
+  return num / 100;
 }
 
 function factorial(...nums) {
@@ -48,6 +52,88 @@ function operate(num1, oper, num2) {
   } else if (oper === "/") {
     return divide(num1, num2);
   }
+}
+
+let input;
+const output = document.querySelector(".screen > p");
+
+const buttons = document.querySelector(".top-buttons");
+
+let isFirstClick = true;
+
+buttons.addEventListener("click", (event) => {
+  const button = event.target;
+
+  if (button.tagName.toLowerCase() !== "button") return;
+
+  if (button.value === "all-clear") {
+    output.textContent = "0";
+    return (isFirstClick = true);
+  }
+
+  if (button.value === "clear") {
+    output.textContent = output.textContent.slice(0, -1) || "0";
+    if (output.textContent === "0") {
+      isFirstClick = true;
+    }
+  } else if (["+", "-", "%", "*", "/"].includes(button.value)) {
+    if (output.textContent === "0") return;
+    else if (/\d$/.test(output.textContent)) {
+      output.textContent += button.textContent;
+    }
+  } else if (button.value === "=") {
+    let expression = output.textContent.split("").join(",");
+    let result = compute(expression);
+    output.textContent = "";
+    console.log(result);
+  } else {
+    if (isFirstClick) {
+      output.textContent = "";
+      isFirstClick = false;
+    }
+    output.textContent += button.textContent;
+  }
+});
+
+function compute(value) {
+  let expression = value.replace(/x/g, "*").replace(/÷/g, "/");
+
+  let formattedExpression = expression.split(",");
+  console.log(formattedExpression);
+  let sum = 0;
+  if (formattedExpression.includes("+")) {
+    let para = [...formattedExpression];
+    para.splice(1, 1);
+    let numericPara = para.map((item) => +item);
+    console.log(numericPara);
+    sum += add(numericPara);
+  } else if (formattedExpression.includes("-")) {
+    let para = [...formattedExpression];
+    para.splice(1, 1);
+    let numericPara = para.map((item) => +item);
+    console.log(numericPara);
+    sum += subtract(numericPara);
+  } else if (formattedExpression.includes("*")) {
+    let para = [...formattedExpression];
+    para.splice(1, 1);
+    let numericPara = para.map((item) => +item);
+    console.log(numericPara);
+    sum += multiply(numericPara);
+  } else if (formattedExpression.includes("/")) {
+    let para = [...formattedExpression];
+    para.splice(1, 1);
+    let numericPara = para.map((item) => +item);
+    console.log(numericPara);
+    sum += divide(numericPara);
+  } else if (formattedExpression.includes("%")) {
+    let para = [...formattedExpression];
+    para.splice(1, 1);
+    let numericPara = para.map((item) => +item);
+    console.log(numericPara);
+    sum += percentage(numericPara);
+  }
+
+  return sum;
 }
 
 // const buttonsBody = document.querySelector(".body");
